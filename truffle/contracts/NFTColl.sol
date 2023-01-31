efaef4666a750c81c198878502494f884c16fb88
Giuseppe-Df <105439679+Giuseppe-Df@users.noreply.github.com>
2023-01-31 13:15:17 +0100

    Update NFTColl.sol

--- truffle/contracts/NFTColl.sol
+++ truffle/contracts/NFTColl.sol
@@ -5,8 +5,25 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFTColl is ERC721URIStorage{
     
+    
     //Time indicators
     mapping(address => uint) winDates;
+    
+    // tranche cannot be collected at this time.
+	error FunctionInvalidAtThisStage();
+    
+    //Time modifier
+	modifier firstTranche(address _partecipant) {
+	if (now <= winDates[_partecipant] + (30 * 1 days))
+	    revert FunctionInvalidAtThisStage();
+	_;
+	}
+    
+    modifier secondTranche(address _partecipant) {
+	if (now <= winDates[_partecipant] + (60 *days))
+	    revert FunctionInvalidAtThisStage();
+	_;
+	}
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
     }
@@ -16,5 +33,7 @@ contract NFTColl is ERC721URIStorage{
         _setTokenURI(id, URI);
         winDates[win]= now;
     }
+    
+    
 
 }
