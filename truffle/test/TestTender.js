4cf9c722eada5281abefa6ed693ea1fec2e4050d
Giuseppe-Df <g.defilippo20@studenti.unisa.it>
2023-02-03 11:01:18 +0100

    implementation of a test tender case

--- truffle/test/TestTender.js
+++ truffle/test/TestTender.js
@@ -1,11 +1,16 @@
 const TenderFactory = artifacts.require("TenderFactory.sol");
-let countTender=0;
+
 contract("TenderFactory", (accounts) => {
     const {expectRevert} = require('@openzeppelin/test-helpers');
   it("Insert new Tender by owner with correct URI", async () => {
     const TenderFatoryInstace = await TenderFactory.deployed();
     const URI = "\{\"name\":\"appalto1\", \"quote\":\"1500\", \"expire\":\"2-02-2023\"\}";
-    assert(await TenderFatoryInstace.openNewTender(URI,{from: accounts[0]}), "A tender with correct URI wasn't created");
+    const tenderInfo=await TenderFatoryInstace.getTenders();
+    var lenghtPreTender= tenderInfo.lenght;
+    await TenderFatoryInstace.openNewTender(URI,{from: accounts[0]});
+    const newTenderInfo=await TenderFatoryInstace.getTenders();
+    var lenghtPostTender= newTenderInfo.lenght;
+    assert.notEqual(lenghtPreTender,lenghtPostTender,"The ADMIN wasn't able to create a new tender with correct URI")
   });
   it("Insert new Tender by owner with wrong URI", async () => {
     const TenderFatoryInstace = await TenderFactory.deployed();
